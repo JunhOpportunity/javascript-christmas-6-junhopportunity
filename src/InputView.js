f4fa0aed51ec7bd16e49f8bf797212a558f61fdb
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-11-13 19:18:36 +0900

    [feat] : Update 사용자의 날짜, 메뉴 입력 함수

--- src/InputView.js
+++ src/InputView.js
@@ -1,16 +1,35 @@
+import { SelectDate, SelectMenu } from "./Select";
+import { Console } from "@woowacourse/mission-utils";
+
 const InputView = {
   async readDate() {
-    const input = await Console.readLineAsync(
-      "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
-    );
-    // ...
+    let date = null;
+    while (!date) {
+      try {
+        const input = await Console.readLineAsync(
+          "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
+        );
+        date = new SelectDate(input);
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+    return date.getValidatedDate();
   },
 
   async readMenu() {
-    const input = await Console.readLineAsync(
-      "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
-    );
-    
+    let menu = null;
+    while (!menu) {
+      try {
+        const input = await Console.readLineAsync(
+          "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
+        );
+        menu = new SelectMenu(input);
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+    return menu.getValidatedMenu();
   },
 };
 
