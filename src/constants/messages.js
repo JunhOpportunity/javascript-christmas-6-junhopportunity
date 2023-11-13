4c7b6ff519a7f810ea6b40e1aad944af98d7ba66
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-11-13 19:16:28 +0900

    [refactor] : Add 주문 유효성 검사에 대한 에러

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,4 +1,6 @@
 const ERROR_DATE_VALIDATE =
   "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+const ERROR_MENU_VALIDATE =
+  "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
 
-export { ERROR_DATE_VALIDATE };
+export { ERROR_DATE_VALIDATE, ERROR_MENU_VALIDATE };
