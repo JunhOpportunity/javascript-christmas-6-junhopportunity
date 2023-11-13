77ea885698d5988947b829899926ce081d9950fc
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-11-13 20:09:45 +0900

    [refactor] : Update 음료만 주문한 경우에 대한 예외 에러 메세지

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -2,5 +2,7 @@ const ERROR_DATE_VALIDATE =
   "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 const ERROR_MENU_VALIDATE =
   "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+const ERROR_ORDER_ONLY_DRINK =
+  "[ERROR] 음료만 주문할 수 없습니다. 다시 입력해 주세요.";
 
-export { ERROR_DATE_VALIDATE, ERROR_MENU_VALIDATE };
+export { ERROR_DATE_VALIDATE, ERROR_MENU_VALIDATE, ERROR_ORDER_ONLY_DRINK };
