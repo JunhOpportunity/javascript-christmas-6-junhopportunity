90479fc1eb8b482745fa0249f0aebecc11a7ce7c
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-11-14 18:32:15 +0900

    [feat] : add 출력 함수 호출 in App.run()

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import Benefit from "./Benefit";
 import InputView from "./InputView";
+import OutputView from "./OutputView";
 
 class App {
   async run() {
     const date = await InputView.readDate();
-    const menu = await InputView.readMenu();
+    const menus = await InputView.readMenu();
+
+    const benefit = new Benefit(date, menus);
+    const benefitRecipt = benefit.get();
+
+    OutputView.printMenu(menus);
+    OutputView.printTotalPaymentBeforeDiscount(benefitRecipt.beforePayment);
+    OutputView.printGiftMenu(benefitRecipt.beforePayment);
+    OutputView.printBenefitDetails(
+      benefitRecipt.beforePayment,
+      benefitRecipt.benefitDetails
+    );
+    OutputView.printTotalBenefit(
+      benefitRecipt.beforePayment,
+      benefitRecipt.totalBenefit
+    );
+    OutputView.printExpectedPayment(benefitRecipt.afterPayment);
+    OutputView.printEventBedge(benefitRecipt.bedge);
   }
 }
 
