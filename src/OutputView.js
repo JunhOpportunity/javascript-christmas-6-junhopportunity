961b1cf75b7cd67efde24dad069a66870bbf5fc9
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-11-14 18:29:33 +0900

    [feat] : Add print 할인 전 총주문 금액 함수

--- src/OutputView.js
+++ src/OutputView.js
@@ -8,6 +8,10 @@ const OutputView = {
       Console.print(`${menu[0]} ${menu[1]}개`);
     });
   },
+  printTotalPaymentBeforeDiscount(beforePayment) {
+    Console.print("<할인 전 총주문 금액>");
+    Console.print(beforePayment.toLocaleString() + "원");
+  },
 };
 
 export default OutputView;
